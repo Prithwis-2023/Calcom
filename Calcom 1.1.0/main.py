e8ea5a41ee890c10d13d34362dd40e117c805f65
Prithwis Das <prithwisdas2017@gmail.com>
2021-12-13 15:34:55 +0530

    remove extra code

--- Calcom 1.1.0/main.py
+++ Calcom 1.1.0/main.py
@@ -1836,20 +1836,3 @@ elif f_l == "n":
 
 time.sleep(10)
 
-from IPython.display import HTML
-
-HTML('''<script>
-code_show=true; 
-function code_toggle() {
- if (code_show){
- $('div.input').hide();
- } else {
- $('div.input').show();
- }
- code_show = !code_show
-} 
-$( document ).ready(code_toggle);
-</script>
-<form action="javascript:code_toggle()"><input type="submit" value="Click here to toggle on/off the raw code."></form>''')
-
-
