c9126f4e4f08d9517c4784ce896eb20c1fb2960f
Prithwis Das <prithwisdas2017@gmail.com>
2022-01-15 15:06:57 +0530

    update generate.sh file

---
+++ generate.sh
@@ -0,0 +1,12 @@
+sudo apt update
+sudo apt install docker.io
+sudo systemctl start docker.service
+sudo systemctl enable docker.service
+sudo docker pull pycalcx/calcom
+sudo apt-get update
+sudo apt-get install x11-xserver-utils
+xhost +
+clear
+sudo docker run -t -i -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=unix$DISPLAY calcom
+
+
