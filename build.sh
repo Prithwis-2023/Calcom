c8baed7fd7b98201641715748d2eb0ea49d85951
Prithwis Das <prithwisdas2017@gmail.com>
2022-02-08 22:29:24 +0530

    update build for ECR

--- build.sh
+++ build.sh
@@ -2,3 +2,7 @@ docker build -t calcom .
 docker login
 docker tag calcom pycalcx/calcom
 docker push pycalcx/calcom
+aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/l7l3b6y2
+docker build -t calcom .
+docker tag calcom:latest public.ecr.aws/l7l3b6y2/calcom:latest
+docker push public.ecr.aws/l7l3b6y2/calcom:latest
