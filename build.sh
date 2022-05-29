390314ef5298b62ab53c232cefa41455906b97c1
Prithwis Das <prithwisdas2017@gmail.com>
2022-05-29 18:37:14 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
-docker build -t calcom .
-docker login
-docker tag calcom pycalcx/calcom
-docker push pycalcx/calcom
-aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/l7l3b6y2
-docker build -t calcom .
-docker tag calcom:latest public.ecr.aws/l7l3b6y2/calcom:latest
-docker push public.ecr.aws/l7l3b6y2/calcom:latest
+sudo docker build -t calcom .
+sudo docker login
+sudo docker tag calcom prithwisdev/calcom
+sudo docker push prithwisdev/calcom
+#aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/l7l3b6y2
+#docker build -t calcom .
+#docker tag calcom:latest public.ecr.aws/l7l3b6y2/calcom:latest
+#docker push public.ecr.aws/l7l3b6y2/calcom:latest
